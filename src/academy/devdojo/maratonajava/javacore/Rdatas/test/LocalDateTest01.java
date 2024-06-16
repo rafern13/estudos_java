d678255377d11af0652a509e425e5bad900d8006
Rafael Fernandes <rafafernandes1308@gmail.com>
2024-06-15 20:04:36 -0400

    Entendendo o LocalDate do java.time

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/LocalDateTest01.java
@@ -0,0 +1,31 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoField;
+import java.util.Calendar;
+import java.util.Date;
+
+public class LocalDateTest01 {
+    public static void main(String[] args) {
+        //LocalDate é a versao mais poderosa
+        //nao pode ser instanciada pois o seu construtor é privado
+        //tranks pa caraio de usar, aparentemente
+        //é threadsafe
+        System.out.println(new Date());
+        System.out.println(Calendar.getInstance());
+        System.out.println(Month.JANUARY.getValue());
+        LocalDate date = LocalDate.of(2022, Month.JANUARY, 27);
+        LocalDate agora = LocalDate.now();
+        System.out.println(date.getYear());
+        System.out.println(date.getMonth());
+        System.out.println(date.getMonthValue());
+        System.out.println(date.getDayOfWeek());
+        System.out.println(date.getDayOfMonth());
+        System.out.println(date.lengthOfMonth());
+        System.out.println(date.isLeapYear());
+        System.out.println(date.get(ChronoField.YEAR_OF_ERA));
+        System.out.println(date);
+        System.out.println(agora);
+    }
+}
