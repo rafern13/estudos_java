e1ab12c7c86dc09d9dc1a2db203ae010b257bd63
Rafael Fernandes <rafafernandes1308@gmail.com>
2024-06-15 20:37:39 -0400

    Entendendo o LocalDateTime do java.time

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/LocalDateTimeTest01.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
+public class LocalDateTimeTest01 {
+    public static void main(String[] args) {
+        //LocalDateTime é a junção de LocalTime e LocalDate
+        //é para qnd voce precisa trabalhar com os dois
+        //vc pode integrar uma 'instancia' de LocalTime ou LocalDate numa LocalDateTime a qualquer momento
+        //parece super util de usar
+        LocalDateTime localDateTime = LocalDateTime.now();
+        LocalDate date = LocalDate.parse("2022-08-06");
+        LocalTime time = LocalTime.parse("09:45:00");
+        System.out.println(localDateTime);
+        System.out.println(date);
+        System.out.println(time);
+        LocalDateTime ldt1 = date.atTime(time);
+        LocalDateTime ldt2 = time.atDate(date);
+        System.out.println(ldt1);
+        System.out.println(ldt2);
+    }
+}
